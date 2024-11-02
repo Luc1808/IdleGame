385461ecc790296ac1555e74abb77e72c4e1daa2
Lucas <520351@student.fontys.nl>
2024-11-02 17:32:06 +0100

    starting again

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,6 +66,7 @@ dependencies {
     implementation(libs.androidx.room.ktx)
 
     implementation(libs.androidx.room.runtime)
+    implementation(libs.firebase.firestore.ktx)
     annotationProcessor(libs.androidx.room.compiler)
     ksp(libs.androidx.room.compiler)
 
--- app/src/main/java/com/example/idlegame/screens/Screen.kt
+++
@@ -1,8 +0,0 @@
-package com.example.idlegame.screens
-
-sealed class Screen(val route: String ) {
-    data object LoginScreen : Screen("loginscreen")
-    data object SignupScreen : Screen("signupscreen")
-    data object UserProfileScreen : Screen("userprofilescreen")
-    data object SkillsScreen : Screen("skillsscreen")
-}
